e243d44ee53fc896bd341d2013e56e8b6a5fe087
Sebastian <sebaacevedo.d@gmail.com>
2022-11-13 19:38:11 -0300

    se actualizo el editar, funciona perfecto

--- editar.php
+++ editar.php
@@ -21,8 +21,6 @@ if (!isset($_GET['id'])) {
     $resultado['mensaje'] = 'La clave no existe';
 }
 
-
-//aca van las sentencias de edición
 ?>
 
 <?php require "templates/header.php"; ?>
@@ -47,51 +45,54 @@ if ($resultado['error']) {
 if (isset($_POST['submit']) && !$resultado['error']){
 
     $dsn = 'mysql:host=' . $config ['db']['host'] . ';dbname=' . $config ['db']['name'];
-    $conexion = new PDO($dsn, $config ['db']['user'], $config ['db']['pass'], $config['db']['options']);
+    $conexion = new PDO($dsn, $config ['db']['user'], $config ['db']['pass'], $config ['db']['options']);
 
-    // capturar valores
-    $nombre_puestero= $_POST['nombre_puestero'];
-    $producto_nombre= $_POST['producto_nombre'];
-    $sub_producto= $_POST['sub_producto'];
-    $cantidad=  $_POST['cantidad'];
-    $tipo= $_POST['tipo'];
-    $peso= $_POST['peso'];
-    $precio_max= $_POST['precio_max'];
-    $precio_min= $_POST['precio_min'];
-    $id_producto= $_POST['id_producto'];
+    //Aca se capturan los valores
+    $nombre_puestero = $_POST['nombre_puestero'];
+    $producto_nombre = $_POST['producto_nombre'];
+    $sub_producto = $_POST['sub_producto'];
+    $cantidad = $_POST['cantidad'];
+    $tipo = $_POST['tipo'];
+    $peso = $_POST['peso'];
+    $precio_max = $_POST['precio_max'];
+    $precio_min = $_POST['precio_min'];
+    $id_producto = $_POST['id_producto'];
 
-    // consulta
+    //UPDATE de consulta SQL
     $consultaSQL = "UPDATE productos SET
     nombre_puestero = :nombre_puestero,
-    producto_nombre= :producto_nombre,
+    producto_nombre = :producto_nombre,
     sub_producto = :sub_producto,
-    cantidad= :cantidad,
-    tipo= :tipo,
-    peso= :peso,
-    precio_max= :precio_max,
+    cantidad = :cantidad,
+    tipo = :tipo,
+    peso = :peso,
+    precio_max = :precio_max,
     precio_min = :precio_min
-    WHERE id_producto= :id_producto";
+    WHERE id_producto = :id_producto";
+
 
     $sentencia = $conexion->prepare($consultaSQL);
 
-    // ligar parametros
+
+    //Parametros
+
     $sentencia->bindParam(':id_producto', $id_producto, PDO::PARAM_INT);
     $sentencia->bindParam(':producto_nombre', $producto_nombre, PDO::PARAM_STR);
     $sentencia->bindParam(':sub_producto', $sub_producto, PDO::PARAM_STR);
     $sentencia->bindParam(':cantidad', $cantidad, PDO::PARAM_INT);
-    $sentencia->bindParam(':tipo', $tipo, PDO::PARAM_INT);
-    $sentencia->bindParam(':peso', $peso, PDO::PARAM_INT);
+    $sentencia->bindParam(':tipo', $tipo, PDO::PARAM_STR);
+    $sentencia->bindParam(':peso', $peso, PDO::PARAM_STR);
     $sentencia->bindParam(':precio_max', $precio_max, PDO::PARAM_STR);
     $sentencia->bindParam(':precio_min', $precio_min, PDO::PARAM_STR);
-    $sentencia->bindParam(':nombre_puestero', $nombre_puestero, PDO::PARAM_INT);
+    $sentencia->bindParam(':nombre_puestero', $nombre_puestero, PDO::PARAM_STR);
 
-    $sentencia->execute();
+    $sentencia -> execute();
 
     ?>
     <div class="container m2-2">
         <div class="col-md-12">
             <div class="alert alert-sucess" role="alert">
-                El producto ha sido actualizado.
+                    El producto se actualizó.
             </div>
         </div>
     </div>
@@ -99,17 +100,20 @@ if (isset($_POST['submit']) && !$resultado['error']){
 }
 ?>
 
-<?php
 
-    // XXX CARGO INFO DEL PRODUCTO
-    $dsn ='mysql:host=' .  $config['db']['host'] . ';dbname=' . $config ['db']['name'];
-    $conexion = new PDO($dsn, $config ['db']['user'], $config['db']['pass'], $config ['db']['options']);
+
+<?php
+    //Info de los productos
+
+    $dsn = 'mysql:host=' . $config ['db']['host'] .';dbname=' . $config ['db']['name'];
+    $conexion = new PDO($dsn, $config ['db']['user'], $config ['db']['pass'], $config ['db']['options']);
 
     $id = $_GET['id'];
     $consultaSQL = "SELECT * FROM productos WHERE id_producto = :id;";
 
     $sentencia = $conexion->prepare($consultaSQL);
-    // ligamos los parametros a la consulta preparada
+
+    //parametros
     $sentencia->bindParam(':id', $id, PDO::PARAM_INT);
 
     $sentencia->execute();
@@ -125,37 +129,44 @@ if (isset($productos) && $productos){
                 <h2 class="mt-4">Edicion de productos y pedidos <?= escapar($productos['nombre_puestero']) ?> </h2>
                 <hr>
                 <form method="post">
-                    <!-- la id del objeto -->
-                    <input type="hidden" name="id_producto" id="id_producto" value="<?= escapar($productos['id_producto']) ?>">
+                    <!---Id producto objeto--->
+                    <input type="hidden" name="id_producto" id="id_producto" value="<?= escapar ($productos['id_producto'])?>">
 
                     <div class="form-group">
                         <label for="nombre">Nombre de Producto</label>
                         <input type="text" name="producto_nombre" id="producto_nombre" value="<?= escapar($productos['producto_nombre']) ?>" class="form-control" required autofocus>
                     </div>
+
                     <div class="form-group">
                         <label for="nombre">Sub Producto</label>
                         <input type="text" name="sub_producto" id="sub_producto" value="<?= escapar($productos['sub_producto']) ?>" class="form-control" required autofocus>
                     </div>
+
                     <div class="form-group">
                         <label for="nombre">Cantidad</label>
                         <input type="text" name="cantidad" id="cantidad" value="<?= escapar($productos['cantidad']) ?>" class="form-control" required autofocus>
                     </div>
+
                     <div class="form-group">
                         <label for="nombre">Tipo</label>
                         <input type="text" name="tipo" id="tipo" value="<?= escapar($productos['tipo']) ?>" class="form-control" required autofocus>
                     </div>
+
                     <div class="form-group">
                         <label for="nombre">Peso</label>
                         <input type="text" name="peso" id="peso" value="<?= escapar($productos['peso']) ?>" class="form-control" required autofocus>
                     </div>
+
                     <div class="form-group">
                         <label for="nombre">Precio Maximo</label>
                         <input type="text" name="precio_max" id="precio_max" value="<?= escapar($productos['precio_max']) ?>" class="form-control" required autofocus>
                     </div>
+
                     <div class="form-group">
                         <label for="nombre">Precio Minimo</label>
                         <input type="text" name="precio_min" id="precio_min" value="<?= escapar($productos['precio_min']) ?>" class="form-control" required autofocus>
                     </div>
+
                     <div class="form-group">
                         <label for="nombre">Nombre Puestero</label>
                         <input type="text" name="nombre_puestero" id="nombre_puestero" value="<?= escapar($productos['nombre_puestero']) ?>" class="form-control" required autofocus>
@@ -172,10 +183,8 @@ if (isset($productos) && $productos){
     </div>
     <?php
 }else{
-  //XXX
-  echo "no hay producto<br>";
+    echo "No hay producto<br>";
 }
-
 ?>
 
 <?php require "templates/footer.php"?>
